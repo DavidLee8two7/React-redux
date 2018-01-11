6295a06ed22621e9c100e34e4fd411c9f370a4e8
David Lee <davidlee8two7@gmail.com>
2018-01-10 17:39:58 -0800

    Implemented search bar

---
+++ src/components/search_bar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+// const SearchBar = () => {
+//     return <input />; // React.createElement
+// };
+
+class SearchBar extends Component {
+    render() {
+        return <input />;
+    }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react'; // var React = require('react');
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyC6yoo5ncA_l1KD5Vv93g8F9RO2bcb0MWI';
 
 // create a new component. This component should produce some HTML
 const App = () => {
-    return <div>Hi you!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );         
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
