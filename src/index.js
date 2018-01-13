664580831f1332230f4b5df8980208d132761535
David Lee <davidlee8two7@gmail.com>
2018-01-13 15:55:40 -0800

    setting video lists

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
-import React from 'react'; // var React = require('react');
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyCPcYQhXKNEI4eC1ofjGUXAODQN7ONU5wI';
 
-const API_KEY = 'AIzaSyC6yoo5ncA_l1KD5Vv93g8F9RO2bcb0MWI';
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-// create a new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );         
+        this.state = { videos: [] };
+        
+        YTSearch({key: API_KEY, term: 'Elon Musk'}, (videos) => {
+            this.setState({ videos });
+            // this.setState({ videos: videos }) works when the key and the value are the same name
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={ this.state.videos } /> 
+            </div>
+        );   // defining the property videos on the JSX
+    }
 }
 
-// Take this component's generated HTML and put it on the page (in the DOM)
-ReactDOM.render(<App />, document.querySelector('.container'));
+
+ReactDOM.render(<App />, document.querySelector('.container'), console.log('callback'));
