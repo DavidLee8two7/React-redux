236c1e93605bedf77bc3b728fb63e03f0d136a9f
DavidLee8two7 <DavidLee8two7@gmail.com>
2018-01-14 17:15:47 -0800

    Adding style & searching option

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
@@ -15,8 +16,11 @@ class App extends Component {
       selectedVideo: null
     };
 
+    this.videoSearch('coding');
+  }
 
-    YTSearch({key: API_KEY, term: 'Elon'}, videos => {
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, videos => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -26,9 +30,11 @@ class App extends Component {
 
 // VideoList component get eccess to it's parent component by defining a property
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch}/>
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
